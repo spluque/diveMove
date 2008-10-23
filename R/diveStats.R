d3d8c834cfca430840c4fb465f6935e3a46ad503
Sebastian Luque <spluque@gmail.com>
2008-10-23 05:24:37 +0000

    Specified row.names=NULL to avoid warning about duplicated row.names.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/divemove/R_pkgs/diveMove/trunk@187 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/diveStats.R
+++ R/diveStats.R
@@ -44,7 +44,7 @@
         ptd <- matrix(c(pdtimes, getSpeed(zvtdr)[okpd]), ncol=2)
         pdv <- do.call(rbind, by(ptd, pdids, diveMove:::.speedStats))
         res <- data.frame(perdive, postdive.dur, postdive.tdist=pdv[, 1],
-                          postdive.mean.speed=pdv[, 2])
+                          postdive.mean.speed=pdv[, 2], row.names=NULL)
         for (i in 1:3) res[, i] <- structure(res[, i],
                                              class=c("POSIXt", "POSIXct"),
                                              tzone=dtimestz)
