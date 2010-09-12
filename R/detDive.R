94a68be010315f92291f6f094ea558423029a0fd
Sebastian Luque <spluque@gmail.com>
2010-09-12 19:17:52 +0000

    Reincorporated quantiles for dive phase detection.
    But they have a slightly different interpretation now.  The quantiles
    refer to the first derivative, which include all the knots used in the
    spline.  This allows us to infer the rates of descent at any number of
    points, based on 'knot.factor'.

--- R/detDive.R
+++ R/detDive.R
@@ -67,15 +67,17 @@
 }
 
 ##_+ Dive Detection with smoothing spline and derivative
-".cutDive" <- function(x, smooth.par, knot.factor)
+".cutDive" <- function(x, smooth.par, knot.factor, descent.crit.q,
+                       ascent.crit.q)
 {
     ## Value: Factor that breaks a dive into descent, descent/bottom,
     ## bottom, bottom/ascent, ascent, and/or descent/ascent given a
     ## proportion of maximum depth for bottom time.  Return a character
     ## matrix with orig ID and corresponding label.
     ## --------------------------------------------------------------------
-    ## Arguments: x=a 3-col matrix with index in original TDR object and
-    ## non NA depths.  A single dive's data; smooth.par=spar parameter for
+    ## Arguments: x=a 3-col numeric matrix with index in original TDR
+    ## object, non-NA depths and numeric time.  A single dive's data
+    ## (*below* 'dive.threshold'); smooth.par=spar parameter for
     ## smooth.spline(); knot.factor=numeric scalar that multiplies the
     ## duration of the dive (used to construct the time predictor for the
     ## derivative)
@@ -88,10 +90,14 @@
                     dive.spline=NULL, spline.deriv1=NULL, descent.crit=NULL,
                     ascent.crit=NULL))
     }
-    depths <- x[, 2]
-    times <- x[, 3]
-    if (nrow(x) >= 4) { # guard against smooth.spline() limitations
-        times.scaled <- as.numeric(times) - as.numeric(times[1])
+    ## We're passed sub-dive threshold observations, so we add 0 at ends to
+    ## get proper derivatives
+    depths <- c(0, x[, 2], 0)
+    times <- c(x[1, 3] - diff(x[, 3])[1],
+               x[, 3],
+               x[nrow(x), 3] + diff(x[, 3])[nrow(x) - 1])
+    if (length(times) >= 4) {   # guard against smooth.spline() limitations
+        times.scaled <- times - times[1]
     } else {
         times.scaledOrig <- as.numeric(times) - as.numeric(times[1])
         times4 <- seq(times[1], times[length(times)], length.out=4)
@@ -107,38 +113,74 @@
     depths.smooth <- stats::smooth.spline(times.scaled, depths,
                                           spar=smooth.par, all.knots=TRUE)
     depths.deriv <- predict(depths.smooth, times.pred, deriv=1)
-    depths.s <- depths.deriv$y
+    depths.d <- depths.deriv$y
 
     ## Descent ------------------------------------------------------------
-    Dd1pos.l <- rle(sign(depths.s))$lengths[1]
-    Dd1pos.min <- which.min(depths.s[seq(Dd1pos.l)])
+    Dd1pos.rle <- rle(sign(depths.d))
+    ## First sequence of positives (descent)
+    Dd1pos.idx <- which.max(Dd1pos.rle$values)
+    ## How long is this sequence from beginning?
+    Dd1pos.sum <- sum(Dd1pos.rle$lengths[seq(Dd1pos.idx)])
+    Dd1.maybe <- depths.d[seq(Dd1pos.sum)]
+    ## Index with first minimum from beginning (only positives)
+    if (all(Dd1.maybe <= 0)) {     # but first maximum if all non-positives
+        Dd1pos.min <- which.max(Dd1.maybe)
+    } else {
+        crit.rate <- quantile(Dd1.maybe, probs=descent.crit.q)
+        beyond <- Dd1.maybe >= crit.rate
+        Dd1pos.min <- ifelse(any(beyond),
+                             which.min(Dd1.maybe[beyond]),
+                             which.min(Dd1.maybe >= 0))
+    }
+    ## Or one could just do this if not worried about starting negative d'
+    ## Dd1pos.l <- rle(sign(depths.d))$lengths[1]
+    ## Dd1pos.min <- which.min(depths.d[seq(Dd1pos.l)])
     Dd1pos.crit <- which.min(abs(times.pred[Dd1pos.min] - times.scaled))
 
     ## Ascent -------------------------------------------------------------
-    ## Length of the sequence of negative derivative at the end of the
-    ## predicted smooth depths (from end)
-    Ad1neg.l <- rle(sign(rev(depths.s)))$lengths[1]
-    ## Position of the *first* maximum negative derivative in the sequence
-    ## at the end of the predicted smooth depths (from end)
-    Ad1neg.max <- which.max(rev(depths.s)[seq(Ad1neg.l)])
-    ## Absolute differences between the time predictor corresponding to the
+    Ad1 <- rev(depths.d)                # work from end of dive
+    Ad1neg.rle <- rle(sign(Ad1))
+    ## First sequence of negatives (ascent)
+    Ad1neg.idx <- which.min(Ad1neg.rle$values)
+    ## How long is this sequence from the end?
+    Ad1neg.sum <- sum(Ad1neg.rle$lengths[seq(Ad1neg.idx)])
+    ## Potential ascent derivatives from end
+    Ad1.maybe <- Ad1[seq(Ad1neg.sum)]
+    ## Potential ascent derivatives in natural order
+    Ad1.maybe.nat <- rev(Ad1[seq(Ad1neg.sum)])
+    ## Index with first maximum in natural ascent order (only negatives)
+    if (all(Ad1.maybe >= 0)) {      # but first minimum if all non-negative
+        Ad1neg.max.nat <- which.min(Ad1.maybe.nat)
+    } else {
+        crit.rate <- quantile(Ad1.maybe.nat, probs=(1 - ascent.crit.q))
+        beyond <- Ad1.maybe.nat <= crit.rate
+        beyond.w <- which(beyond)    # indices below critical in candidates
+        beyond0 <- Ad1.maybe.nat <= 0
+        beyond0.w <- which(beyond0) # indices of non-positives in candidates
+        Ad1neg.max.nat <- ifelse(any(beyond),
+                                 beyond.w[which.max(Ad1.maybe.nat[beyond])],
+                                 beyond0.w[which.max(Ad1.maybe.nat[beyond0])])
+    }
+    ## Position of *first* maximum negative derivative in the sequence at
+    ## the end of the predicted smooth depths (from end)
+    Ad1neg.max <- Ad1neg.sum - Ad1neg.max.nat + 1
+    ## Absolute differences between time predictor corresponding to the
     ## position above and scaled time (it's important it's in reverse, so
     ## that we can find the first minimum below)
     Ad1neg.ad <- abs(rev(times.pred)[Ad1neg.max] - rev(times.scaled))
     ## Position of the *first* minimum absolute difference above
     Ad1neg.crit <- length(times.scaled) - which.min(Ad1neg.ad) + 1
 
-    ## Correct both descent and ascent critical indices if this is a brief
-    ## dive
-    if (nrow(x) < 4) {
+    ## Correct both critical indices if this is a brief dive
+    if (length(times) < 4) {
         Dd1pos.crit <- which.min(abs(times.scaled[Dd1pos.crit] -
                                      times.scaledOrig))
         Ad1neg.crit <- which.min(abs(times.scaled[Ad1neg.crit] -
                                      times.scaledOrig))
     }
-
-    descind <- seq(Dd1pos.crit)
-    ascind <- seq(Ad1neg.crit, nrow(x))
+    ## Correct for added 0 at ends
+    descind <- seq(Dd1pos.crit - 1)
+    ascind <- seq(Ad1neg.crit - 1, length(times) - 2)
 
     ## Bottom -------------------------------------------------------------
     bottind <- c(descind[length(descind)],
@@ -166,8 +208,10 @@
     labs[a] <- "A"
     labs[da] <- "DA"
     ## If there are repetitions, keep the last one to avoid missing ascent labels
-    rowids <- unique(c(x[d, 1], x[db, 1], x[b, 1], x[ba, 1], x[a, 1], x[da, 1]))
-    label.mat <- cbind(rowids, labs)
+    rowids <- unique(c(x[d, 1], x[db, 1], x[b, 1], x[ba, 1], x[a, 1], x[da, 1]),
+                     fromLast=TRUE)
+    ## Any remaining rowids are nonexistent labels
+    label.mat <- cbind(rowids[!is.na(rowids)], labs[!is.na(rowids)])
     list(label.matrix=label.mat, dive.spline=depths.smooth,
          spline.deriv1=depths.deriv, descent.crit=Dd1pos.crit,
          ascent.crit=Ad1neg.crit)
@@ -227,3 +271,10 @@
 
 
 ## TEST ZONE --------------------------------------------------------------
+
+## diveX <- as.data.frame(extractDive(tdr.calib, diveNo=100))
+## phases <- .cutDive(cbind(as.numeric(row.names(diveX[-c(1, nrow(diveX)), ])),
+##                          diveX$depth[-c(1, nrow(diveX))],
+##                          diveX$time[-c(1, nrow(diveX))]),
+##                    smooth.par=0.1, knot.factor=20, descent.crit.q=0,
+##                    ascent.crit.q=0.5)
